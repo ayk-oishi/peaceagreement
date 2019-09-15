687e895105a32e96921fe3800fdd95b5079836c5
Ayaka Oishi <ayakaoishi@Ayaka-no-MacBook-Air.local>
2019-09-15 17:39:50 -0400

    I updated

--- app.py
+++ app.py
@@ -8,12 +8,11 @@ import pandas as pd
 
 tabtitle = 'Peace Agreements 1990-2016'
 myheading1 = "Peace Agreements 1990-2016"
-myheading2 = 'The number of agreements are decreasing'
+myheading2 = 'There are about 1500 Peace Agreements from 1990 to 2016. The number of agreements are decreasing. \nAs of 2018  "Pre" agreements, meaning the process is under negotiation is common in Asia and Europe. \nContrary, in Africa, "SubPar", which is partially agreed peace process is dominant.'
 image1 = 'Agreements_byYear.png'
 image2 = 'Agreements_byRegionState.png'
-textbody = "There are about 1500 Peace Agreements from 1990 to 2016."
 sourceurl = 'https://www.kaggle.com/university-of-edinburgh/peace-agreements-dataset'
-githublink = 'https://github.com/ayk-oishi/dash-dc-layout'
+githublink = 'https://github.com/ayk-oishi/peaceagreement'
 
 ########### Initiate the app
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
@@ -24,29 +23,18 @@ app.title=tabtitle
 ########### Set up the layout
 app.layout = html.Div(children=[
     html.H2(myheading1),
-    html.H3(myheading2),
+    html.H5(myheading2),
     html.Div([
         html.Div([
-            html.Img(src=app.get_asset_url(image1), style={'width': '50%', 'height': 'auto'})
+            html.Img(src=app.get_asset_url(image1), style={'width': '250%', 'height': 'auto'})
         ],className='three columns'),
         html.Div([
-            html.Img(src=app.get_asset_url(image2), style={'width': '80%', 'height': 'auto'}),
+            html.Img(src=app.get_asset_url(image2), style={'width': '250%', 'height': 'auto'}),
         ],className='three columns'),
-        html.Div([
-            html.Div(textbody, style={
-                'padding': '22px',
-                'font-size': '22px',
-                'height': '120px',
-                'border': 'thin red solid',
-                'color': 'rgb(155, 255, 355)',
-                'backgroundColor': 'rgb(57, 83, 107)',
-                'textAlign': 'left',
-                }),
-        ],className='six columns'),
     ],className='twelve columns'),
     html.A('Code on Github', href=githublink),
     html.Br(),
-    html.A("Data Source", href=sourceurl),
+    html.A("Data Source", href=sourceurl)
     ]
 )
 
